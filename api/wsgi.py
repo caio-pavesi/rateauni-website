2f74330e92d4d83ddc7504b67b53b8929af77538
caio-pavesi <ocaiopavesi@hotmail.com>
2024-09-24 22:43:26 -0300

    docs: Comments referencing vercel docs

--- api/wsgi.py
+++ api/wsgi.py
@@ -13,4 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
 
-app = get_wsgi_application()
+app = get_wsgi_application() # ! - Leave as 'app' (Vercel configuration), see references in docs
