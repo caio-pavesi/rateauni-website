12e7437674c47603a23f94a97c86e4cbce656445
caio-pavesi <ocaiopavesi@hotmail.com>
2024-10-04 17:57:06 -0300

    fix: Campus database removed
    After looking at the Brazilian data, it was found that no Campus data is made available, and obtaining this information would be a hell, so no more campus data.
    
    Also added a column name 'university_acronym' to get commom short names for universities like 'UDESC' (Brazil) or 'MIT' (USA)

--- rates/models.py
+++ rates/models.py
@@ -10,7 +10,6 @@ class Rates(models.Model):
         verbose_name = "rate_id",
         primary_key = True,
         default = uuid.uuid4,
-        help_text = "Unique identifier for the rate",
     )
     rate_date = models.DateTimeField(
         verbose_name = "rate_date",
@@ -20,16 +19,17 @@ class Rates(models.Model):
     rate_email = models.CharField(
         verbose_name = "rate_email",
         max_length = 255,
-        help_text = "Email of the user who rated, 'Anonymous' if skipped validation",
+        null = True,
+        help_text = "Email of the user who rated, 'NULL' if skipped validation",
     )
     rate_validated = models.BooleanField(
         verbose_name = "rate_validated",
         default = False,
         help_text = "If the email was validated or not, see the user flow diagram",
     )
-    rate_campus_id = models.ForeignKey(
-        verbose_name = "rate_campus_id",
-        to = "Campuses",
+    rate_university_id = models.ForeignKey(
+        verbose_name = "rate_university_id",
+        to = "Universities",
         on_delete = models.DO_NOTHING,
     )
     rate_value = models.IntegerField(
@@ -43,55 +43,47 @@ class Universities(models.Model):
     university_id = models.IntegerField(
         verbose_name = "university_id",
         primary_key = True,
-        help_text = "Unique identifier for the university",
+    )
+    university_source_id = models.CharField(
+        verbose_name = "university_source_id",
+        max_length = 255,
+        help_text = (
+            "Unique identifier for the university in the source data "
+            "usually the source API or the data provider (e.g: government)"
+        ),
     )
     university_name = models.CharField(
         verbose_name = "university_name",
         max_length = 255,
         help_text = "Name of the university",
     )
-    university_website = models.CharField(
-        verbose_name = "university_website",
+    university_acronym = models.CharField(
+        verbose_name = "university_acronym",
         max_length = 255,
-        help_text = "Url of the university website",
-    )
-
-class Campuses(models.Model):
-    """Represent a university campus"""
-
-    campus_id = models.IntegerField(
-        verbose_name = "campus_id",
-        primary_key = True,
-        help_text = "Unique identifier for the campus",
-    )
-    campus_university_id = models.ForeignKey(
-        verbose_name = "campus_university_id",
-        to = "Universities",
-        on_delete = models.CASCADE,
-    )
-    campus_name = models.CharField(
-        verbose_name = "campus_name",
-        max_length = 255,
+        null = True,
         help_text = (
-            "Name of the campus, if it own only one campus it "
-            "should be null, filled if more than one exists"
+            "Acronym of the university e.g: 'MIT' for 'Massachusetts Institute of Technology'"
         ),
     )
-    campus_country = models.CharField(
-        verbose_name = "campus_country",
+    university_country = models.CharField(
+        verbose_name = "university_country",
         max_length = 255,
+        help_text = "Country of the university in ISO 3166-1 name (English) format",
+    )
+    university_state = models.CharField(
+        verbose_name = "university_state",
+        max_length = 255,
+        null = True,
         help_text = (
-            "Country of the campus in ISO 3166-1 name format, "
-            "see more in https://en.wikipedia.org/wiki/ISO_3166-1"
+            "State of the university, middle for the contry to in "
+            "ISO 3166-2 or as definied by the source data"
         ),
     )
-    campus_state = models.CharField(
-        verbose_name = "campus_state",
+    university_city = models.CharField(
+        verbose_name = "university_city",
         max_length = 255,
-        help_text = "State of the campus",
-    )
-    campus_city = models.CharField(
-        verbose_name = "campus_city",
-        max_length = 255,
-        help_text = "City of the campus",
+        help_text = (
+            "City of the university, lower for the contry to in "
+            "ISO 3166-2 or as definied by the source data"
+        ),
     )
